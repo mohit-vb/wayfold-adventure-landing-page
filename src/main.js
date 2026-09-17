d4a12106e56997c071678d137e1b735dbcbf0835
mohit-vb <mohitvbjadhav@gmail.com>
2026-09-17 20:48:18 +0530

    feat : replace gsap individual animation with timeline

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 "use strict";
 import { gsap } from "gsap";
 
-gsap.from(".hero__textbox > *", {
-  y: 40,
-  opacity: 0,
-  duration: 0.8,
-  ease: "power3.out",
-  stagger: 0.25,
-});
+let heroTl = gsap.timeline({ defaults: { opacity: 0, ease: "back" } });
+heroTl
+  .from(".hero__textbox .eyebrow", { x: 100, duration: 1.6 })
+  .from(".hero__textbox h1", { x: -100, duration: 1.2 }, "<")
+  .from(".hero__textbox p", { y: 30, duration: 0.8 })
+  .from(".hero__textbox button", { y: 40, duration: 0.8 });
